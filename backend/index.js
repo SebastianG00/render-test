a0319ba3669f0dc99813f13f052ce696d91ae192
Sebastian A. Gutierrez Sheen <sebastian.ags00@berkeley.edu>
2025-04-03 17:30:52 -0700

    database integratio with erro handling

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.json())
 //To make epress show static content, we need to tell it where to find the static files.
 app.use(express.static('dist'))
+
+const Note = require('./models/note')
 
 const cors = require('cors')
 app.use(cors())
@@ -14,61 +17,61 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name == "ValidationError") {
+        return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+}
 
 app.use(requestLogger)
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy, but boring",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Testing Server</h1>')
 })
   
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 
-app.get('/api/notes/:idNumber', (request, response) => {
-    const id = request.params.idNumber
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+      
+    }).catch(error => {
+        next(error)})
+    
+ })
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-})
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//       ? Math.max(...notes.map(n => Number(n.id)))
+//       : 0
+//     return String(maxId + 1)
+// }
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     
     const body = request.body
 
@@ -76,15 +79,35 @@ app.post('/api/notes', (request, response) => {
         return response.status(400).json({ error: 'content missing' })
     }
     
-    const note = {
+    const note = Note({
         content: body.content,
-        important : body.important || false,
-        id: generateId()
-    }
+        important: body.important || false,
+    })
     
-    notes = notes.concat(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    }).catch(error => {
+        next(error) 
+    })
+})
 
-    response.json(note)
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+  
+    Note.findById(request.params.id)
+      .then(note => {
+        if (!note) {
+          return response.status(404).end()
+        }
+  
+        note.content = content
+        note.important = important
+  
+        return note.save().then((updatedNote) => {
+          response.json(updatedNote)
+        })
+      })
+      .catch(error => next(error))
 })
 
 //This middleware will be used for This middleware handles requests to any routes that haven’t been defined (i.e., non-existent routes).
@@ -93,7 +116,7 @@ const unkownEndPoint = (request, response) => {
     response.status(404).send({ error: 'unkown endpoint' })
 }
 app.use(unkownEndPoint)
-
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
